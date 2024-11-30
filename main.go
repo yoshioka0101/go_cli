63ee85f0feffbe618d0ffa851b655be4be8ca5e7
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-11-30 19:50:14 +0900

    数字によって出力を変える

--- main.go
+++ main.go
@@ -4,10 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin) // 標準入力を受け付けるスキャナ
-	scanner.Scan() // １行分の入力を取得する
-	fmt.Println(scanner.Text())
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("1:単語入力")
+		fmt.Println("2:辞書登録リスト")
+		fmt.Println("3:終了")
+
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	switch input {
+	case "1":
+		fmt.Println("単語入力")
+	case "2":
+		fmt.Println("選択した辞書リストです")
+	case "3":
+		fmt.Println("終了します")
+		return
+	default:
+		fmt.Println("無効な数字が入力されました")
+	}
+  }
 }
